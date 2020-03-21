c4ffc10dea2011017a42ad44513add86d458f583
milenoss <milenkhanal@gmail.com>
2020-03-21 17:49:35 +0000

    Building json HTTP endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -43,9 +45,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather',(req, res) => { 
-    res.send({
-        forecast: 'It is cold',
-        location: 'London'
+    if (!req.query.address){ 
+        return res.send({ 
+            error: 'You must provide an address!'
+        })
+
+    }
+    geocode(req.query.address,(error, {latitude, longitude, location}) => { 
+
+     if(error) { 
+         return res.send({ error})
+     }
+
+     forecast(latitude, longitude, (error, forecastData) => { 
+        if (error) { 
+            return res.send({error})
+        }
+        res.send({
+            forecast: forecastData, 
+            location, 
+            address: req.query.address
+         })
+       })
     })
 })
 
@@ -56,6 +77,21 @@ app.get('/help/*', (req, res) => {
         name: 'Milen Khanal',
         errorMessage: 'Help article not found'
 
+    })
+})
+
+
+
+
+app.get('/products',(req, res) => { 
+    if (!req.query.search) { 
+       return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    // console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
